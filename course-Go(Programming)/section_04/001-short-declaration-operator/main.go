b84c11ce874f3a7abdf5bfa237cad7217bf7ee6c
carrenolg <carrenolg@hotmail.com>
2020-11-04 11:06:16 -0500

    short declaration operator

---
+++ course-Go(Programming)/section_04/001-short-declaration-operator/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 30 // (:=) short declaration operator
+	fmt.Println(x)
+	x = 60 // (=) equal operator
+	fmt.Println(x)
+	sum := 100 + 24 // this is a statement example
+	fmt.Println(sum)
+}
