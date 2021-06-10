052a257bbe7c682bc17e56b5c2fb131deb49bdab
Carrenolg <carrenolg@gmail.com>
2021-06-10 17:58:25 -0500

    get ready to start programming in Go

---
+++ courses/1.Go(golang)/section_28/foo/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{1, 2, 3}
+	fmt.Println(sum(s...))
+}
+
+func sum(n ...int) int {
+	sum := 0
+	for _, v := range n {
+		sum += v
+	}
+	return sum
+}
---
+++ courses/2.Building an API Web Server(2018)/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Running API v1\n"))
+}
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	err := http.ListenAndServe("localhost:11111", nil)
+	if err != nil {
+		// panic(err)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
