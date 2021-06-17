b67558ceefa0a1bb5cb96c048a7667196d479d98
Carrenolg <carrenolg@gmail.com>
2021-06-17 12:01:21 -0500

    lab slices

--- courses/3.DueTheBuilder(2019)/008-slices/main.go
+++ courses/3.DueTheBuilder(2019)/008-slices/main.go
@@ -1,17 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// slices declaration
-	var s []int // nil
+	var s []int // nil zero value
 	imprime(s, "s")
-	// slice literal
-	var p = []int{1, 2, 3}
-	imprime(p, "p")
 	// literal empty, no nil
 	var q = []int{}
 	imprime(q, "q")
+	// slice literal
+	var p = []int{1, 2, 3}
+	imprime(p, "p")
 
 	// create new slice from slice
 	var a = []int{0, 1, 2, 3, 4, 5}
@@ -32,6 +34,42 @@ func main() {
 	imprime(child, "child")
 
 	// capacity of slice
+	var w = []int{0, 1, 2, 3, 4, 5}
+	w1 := w[1:4]
+	imprime(w, "w")
+	imprime(w1, "w1")
+	// we extende capacity by 1
+	w1 = w1[:len(w1)+1]
+	w1 = w1[:cap(w1)]
+	imprime(w1, "w1")
+
+	// append func
+	w = append(w, 11, 12, 13, 14)
+	imprime(w, "w")
+
+	// make
+	var m = []int{0, 1, 2, 3, 4, 5}
+	l := make([]int, 6)
+	copy(l, m) // l = m
+	imprime(m, "m")
+	imprime(l, "l")
+	m[2] = 100
+	imprime(m, "m")
+	imprime(l, "l")
+	// m and l have different memory location
+	// fmt.Println(m == l) # invalido: slice can only be compared to nil
+
+	fmt.Println("\nLab #######")
+
+	// reference to an underlying array
+	// slice has its own reference, this isn't same to underlying array
+	var r = []int{15, 25, 35, 45, 55}
+	f := r[1:3]
+	fmt.Printf("ref(r): %p\n", &r) // slice self-reference
+	fmt.Printf("ref(f): %p\n", &f) // slice self-reference
+	r[2] = 300
+	imprime(r, "r")
+	imprime(f, "f")
 
 }
 
