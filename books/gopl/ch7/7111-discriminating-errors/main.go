4f51c38198c11b203b8c801725ae7bb26105c635
Carrenolg <carrenolg@gmail.com>
2021-09-03 13:04:16 -0500

    working on goroutine in Go

--- books/gopl/ch7/7111-discriminating-errors/main.go
+++ books/gopl/ch7/7111-discriminating-errors/main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	// 7.11 Discriminating Errors with Type Assertions
+	_, err := os.Open("/no/such/file")
+	fmt.Println(err)
+	fmt.Printf("%#v\n", err)
+	// type: *os.PathError
 
+	// check what type error is
+	_, err = os.Open("/no/such/file")
+	fmt.Println(os.IsNotExist(err)) // "true"
 }
---
+++ books/gopl/ch8/81-goroutine/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// 8.1 Goroutine
+}
