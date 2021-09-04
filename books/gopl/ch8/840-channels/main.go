5fb981355fe39e28a0ab782d29668bc465937a54
Carrenolg <carrenolg@gmail.com>
2021-09-03 21:59:29 -0500

    working on Channels in GO

---
+++ books/gopl/ch8/840-channels/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// 8.4 Channels
+}
---
+++ books/gopl/ch8/netcat3/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	// netcat3
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+
+	go func() {
+		io.Copy(os.Stdout, conn)
+		log.Println("done")
+		done <- struct{}{} // signal the main goroutine
+	}()
+	mustCopy(conn, os.Stdin)
+	conn.Close()
+	// done
+	<-done
+}
