2d54e57e3b3bd6a9746377ad3d95837f66c25601
carrenolg <carrenolg@gmail.com>
2021-11-25 14:34:13 -0500

    API  with mysql database

---
+++ articles/api-mysql/source/db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `post` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `content` varchar(45) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
