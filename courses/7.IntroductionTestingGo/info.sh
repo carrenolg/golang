c344d4506827b7b39864219e21bd844e8bdebd64
Luis Carreno <carrenolg@gmail.com>
2023-02-02 09:56:16 -0500

    primeapp is finished, all tests ok

--- courses/7.IntroductionTestingGo/info.sh
+++ courses/7.IntroductionTestingGo/info.sh
@@ -1,5 +1,6 @@
 # Section 3 - Simple Testing
 $ go test -v # run all tests
+$ go test -cover -v # show the % coverage
 # coverage report
 $ go test -coverprofile=coverage.out
 $ go tool cover -html=coverage.out
--- courses/7.IntroductionTestingGo/sec03/001-simple-testing/mian_test.go
+++ courses/7.IntroductionTestingGo/sec03/001-simple-testing/mian_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"io"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -49,4 +52,74 @@ func Test_prompt(t *testing.T) {
 		t.Errorf("incorrect prompt: expected -> but got %s", string(out))
 	}
 
+}
+
+func Test_intro(t *testing.T) {
+	// save a copy of os.Stdout
+	oldOut := os.Stdout
+
+	// create a read and write pipe
+	r, w, _ := os.Pipe()
+
+	// set os.Stdout to our write pipe
+	os.Stdout = w
+
+	intro()
+
+	// close our writer
+	_ = w.Close()
+
+	// reset os.Stdout to what it was before
+	os.Stdout = oldOut
+
+	// read the output of our prompt() func from our read pipe
+	out, _ := io.ReadAll(r)
+
+	// perform our test
+	if !strings.Contains(string(out), "Enter a whole number") {
+		t.Errorf("Intro text not correct; got %s", string(out))
+	}
+}
+
+func Test_checkNumbers(t *testing.T) {
+	input := strings.NewReader("q")
+	reader := bufio.NewScanner(input)
+	res, _ := checkNumbers(reader)
+
+	if !strings.EqualFold(res, "") {
+		t.Error("Keyword worng for exit program!")
+	}
+
+	// convert user input to int
+	input = strings.NewReader("1.0")
+	reader = bufio.NewScanner(input)
+	res, _ = checkNumbers(reader)
+	if res != "Please enter a whole number!" {
+		t.Error("Input can't be converted into integer")
+	}
+
+	input = strings.NewReader("7")
+	reader = bufio.NewScanner(input)
+	res, expected := checkNumbers(reader)
+	if expected {
+		t.Errorf("expected '7 is a prime number!', and got %s", res)
+	}
+}
+
+func Test_ReadUserInput(t *testing.T) {
+	// create chan
+	doneChan := make(chan bool)
+
+	// create buffer
+	var stdin bytes.Buffer
+
+	// write in buffer
+	stdin.Write([]byte("1\nq\n"))
+
+	// launch goroutine
+	go readUserInput(&stdin, doneChan)
+
+	// wait to goroutine ending
+	<-doneChan
+	close(doneChan)
 }
