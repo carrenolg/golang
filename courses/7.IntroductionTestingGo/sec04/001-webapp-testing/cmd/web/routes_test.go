33f732350527225bc29f4016ff90a20660f7e6a0
Luis G. Carreno Ortiz <carrenolg@gmail.com>
2023-09-29 15:26:50 -0500

    testing app routes

---
+++ courses/7.IntroductionTestingGo/sec04/001-webapp-testing/cmd/web/routes_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func Test_application_routes(t *testing.T) {
+	// arrays with routes
+	var registered = []struct {
+		route  string
+		method string
+	}{
+		{"/", "GET"},
+	}
+	// create a new variable of type app
+	var app application
+	mux := app.routes()
+	chiRoutes := mux.(chi.Routes)
+
+	for _, route := range registered {
+		// check if the route exits
+		if !routeExists(route.route, route.method, chiRoutes) {
+			t.Errorf("route %s is not registered", route.route)
+		}
+	}
+
+}
+
+func routeExists(testRoute, testMethod string, chiRoutes chi.Routes) bool {
+	found := false
+	walkFn := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		if strings.EqualFold(method, testMethod) && strings.EqualFold(route, testRoute) {
+			found = true
+		}
+		return nil
+	}
+	_ = chi.Walk(chiRoutes, walkFn)
+	return found
+}
