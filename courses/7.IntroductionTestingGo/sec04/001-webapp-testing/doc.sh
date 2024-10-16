155dfdc58679a534f388d25f8d62dea93bcc315b
Luis G. Carreno Ortiz <luis.carreno@mercadolibre.com.co>
2024-10-16 16:13:39 -0500

    add docu about dbrepo testing

--- courses/7.IntroductionTestingGo/sec04/001-webapp-testing/doc.sh
+++ courses/7.IntroductionTestingGo/sec04/001-webapp-testing/doc.sh
@@ -4,3 +4,9 @@ go test --cover
 go test -coverprofile=coverage.out
 # generate html file
 go tool cover -html=coverage.out -o coverage.html
+# create users.sql
+touch pkg/repository/dbrepo/testdata/users.sql
+# setup colima with dockertest
+sudo ln -s /Users/{user_name}/.colima/default/docker.sock /var/run/docker.sock
+# keep in mind change your user name
+# https://github.com/testcontainers/testcontainers-java/issues/5034
